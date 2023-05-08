9d55a06c8183d339e0c4e89d102e04629b2632eb
Xheve <xk64655@ubt-uni.net>
2023-05-08 18:50:18 +0200

    home page added, header and footer

--- src/partials/Footer/Footer.jsx
+++ src/partials/Footer/Footer.jsx
@@ -1,9 +1,24 @@
 import * as React from "react";
+import { NavLink } from "react-router-dom";
 
 const Footer = () => {
+  const year = new Date().getFullYear();
   return (
-    <footer>
-      <h3>Footer</h3>
+    <footer className="footer">
+      <div className="container">
+        <div className="footer-part1">
+          <NavLink to="./" className="headerIcon">
+            ADEX
+          </NavLink>
+          <div className="links">
+            <NavLink to="/cars">Cars</NavLink>
+            <NavLink to="/faq">FAQs</NavLink>
+          </div>
+        </div>
+        <p className="footer-part2">
+          &#169;{`Copyright ${year}. All rights reserved.`}
+        </p>
+      </div>
     </footer>
   );
 };
