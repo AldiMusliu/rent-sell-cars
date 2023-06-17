fb867ebfa479e8c758845edf41691014ed24bae6
Aldi <aldimusliu5@gmial.com>
2023-06-17 17:07:21 +0200

    form fixed

--- src/pages/Car/Car.jsx
+++ src/pages/Car/Car.jsx
@@ -1,11 +1,12 @@
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 import { api } from "../../lib/api/";
 import React from "react";
-import {useState} from 'react'
+import { useState } from "react";
 import CostumModal from "../../components/CostumModal";
 import BookNowModalContent from "../../components/BookNowModalContent/BookNowModalContent";
 
 const Car = () => {
+  const navigate=useNavigate()
   const { carId } = useParams();
   const [car, setCar] = React.useState();
   const getCar = async () => {
@@ -15,6 +16,22 @@ const Car = () => {
     }
   };
   const [modalShow, setModalShow] = useState(false);
+  const handleBookNowBtnClick=()=>{
+    if(localStorage.getItem('token')){
+      setModalShow(true)
+    }
+    else(
+      navigate('/login')
+    )
+  }
+  const handleBuyNowBtnClick=()=>{
+    if(localStorage.getItem('token')){
+
+    }
+    else(
+      navigate('/login')
+    )
+  }
   React.useEffect(() => {
     getCar();
   }, []);
@@ -23,7 +40,7 @@ const Car = () => {
       <h4 className="car-title">{car?.name}</h4>
       <div className="car-content">
         <div className="car-content-details">
-        <h5>{car?.description}</h5>
+          <h5>{car?.description}</h5>
           <p>
             <span>Model: </span>
             {car?.model}
@@ -49,15 +66,11 @@ const Car = () => {
         <img src={car?.images} alt="" className="car-content-photo" />
       </div>
       <div className="car-buttons">
-        <button onClick={()=>setModalShow(true)}>Book Now</button>
-        <button>Buy Now</button>
+        <button onClick={handleBookNowBtnClick}>Book Now</button>
+        <button onClick={handleBuyNowBtnClick}>Buy Now</button>
       </div>
       <CostumModal
-        modalBody={
-          <BookNowModalContent
-          setModalShow={setModalShow}
-        />
-        }
+        modalBody={<BookNowModalContent setModalShow={setModalShow} />}
         show={modalShow}
         onHide={() => setModalShow(false)}
       />
